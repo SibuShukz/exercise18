f7d32386c233faf2e29031bf22a8854f7f82a85e
SibuShukz <saurabh180501@gmail.com>
2024-06-29 15:59:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [cards, setCards] = useState([])
+
+  const fetchData=async ()=>{
+    let a =await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data =await a.json()
+    setCards(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+
+  return (
+    <>
+      <div className="container">
+      {cards.map((card)=>{
+        return <div key={card.userId} className="card">
+          <h2>{card.title}</h2>
+          <p>{card.body}</p>
+          <span>By: UserId: {card.userId}</span>
+        </div>
+      })}
+       
+      </div>
+    </>
+  )
+}
+
+export default App
